b85779f450d98503d9682726dbee49ddcf3a225c
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-13 19:23:51 -0800

    adding fontawesome-free-solid for dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,13 +2,24 @@
 'use strict'
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
+const Funnel = require('broccoli-funnel');
+const path = require('path');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
+    trees: {
+      vendor: new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome-free-solid')), {
+        destDir: 'fontawesome-free-solid'
+      })
+    }
   })
 
   app.import('vendor/shims/fontawesome-shim.js')
+
+
+  app.import('vendor/fontawesome-free-solid/index.js')
+  app.import('vendor/shims/fontawesome-free-solid-shim.js')
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
---
+++ vendor/shims/fontawesome-free-solid-shim.js
@@ -0,0 +1,12 @@
+(function() {
+  function vendorModule() {
+    'use strict';
+
+    return {
+      'default': self['fontawesome-free-solid'],
+      __esModule: true,
+    };
+  }
+
+  define('@fortawesome/fontawesome-free-solid', [], vendorModule);
+})();
