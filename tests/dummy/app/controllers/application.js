a582d70df10ae6a622664e9a3a9d557dedd40ff9
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-15 13:28:22 -0800

    add spinning magic demo for both potential implementations.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,10 +3,21 @@ import fontawesome from '@fortawesome/fontawesome'
 import fas from '@fortawesome/fontawesome-free-solid'
 
 // Make some of our icons available by their names as strings by adding them to the library
-fontawesome.library.add(fas.faCog, fas.faCircle, fas.faSquare)
+fontawesome.library.add(fas.faCog, fas.faCircle, fas.faSquare, fas.faMagic)
 
 export default Controller.extend({
   // Make a couple of icons available via object reference in our templates
   faCoffee: fas.faCoffee,
-  faSquare: fas.faSquare
+  faSquare: fas.faSquare,
+  faMagic: fas.faMagic,
+  init(){
+    this.set('magic',0)
+    this.set('magic-alt',0)
+  },
+  actions: {
+    updateMagic(val){
+      this.set('magic',val)
+      this.set('magic-alt',val)
+    }
+  }
 });
