dfcbce2a88907cc0f10e31fbaa8acc94a83a2c2a
Mike Wilkerson <mwilkerson@gmail.com>
2017-12-15 13:51:36 -0800

    demonstrating fixedWidth support

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,7 +3,16 @@ import fontawesome from '@fortawesome/fontawesome'
 import fas from '@fortawesome/fontawesome-free-solid'
 
 // Make some of our icons available by their names as strings by adding them to the library
-fontawesome.library.add(fas.faCog, fas.faCircle, fas.faSquare, fas.faMagic)
+fontawesome.library.add(
+  fas.faCog,
+  fas.faCircle,
+  fas.faSquare,
+  fas.faMagic,
+  fas.faHome,
+  fas.faInfo,
+  fas.faBook,
+  fas.faPencilAlt,
+  )
 
 export default Controller.extend({
   // Make a couple of icons available via object reference in our templates
