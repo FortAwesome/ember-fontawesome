4a9c6f0e155ff92ba6402f3d8aad3a1cfc9fe3a3
Mike Wilkerson <mwilkerson@gmail.com>
2018-02-09 23:52:47 -0800

    WIP: got tree shaking working with rollup for icon packs

--- index.js
+++ index.js
@@ -5,51 +5,39 @@ var MergeTrees = require('broccoli-merge-trees')
 var path = require('path')
 var debug = require('broccoli-stew').debug
 var Rollup = require('broccoli-rollup')
-var alias = require('rollup-plugin-alias')
 var fs = require('fs')
+var rimraf = require('rimraf');
+var resolve = require('rollup-plugin-node-resolve')
 
 module.exports = {
   name: 'ember-fontawesome',
 
-//  treeForApp(appTree) {
-//    return debug(
-//      new MergeTrees([
-//        appTree,
-//        new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome')), {
-//          destDir: 'fontawesome'
-//        }),
-//        new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome-free-solid')), {
-//          destDir: 'fontawesome-free-solid'
-//        })
-//      ]), 
-//      {name: 'ember-font-awesome-app-tree'}
-//    )
-//  },
-
   treeForVendor(vendorTree) {
     const packageContents = `
-      //export { ${this.fontawesome.fas.join(',')} }  from '@fortawesome/fontawesome-free-solid'
-      console.log('foo')
+      export { ${this.fontawesome.fas.join(',')} }  from '@fortawesome/fontawesome-free-solid/shakable.es.js'
     `
-    const libTemp = fs.mkdtempSync('ember-fontawesome-pack-')
+    this._tmpDir = `${this.project.root}/${fs.mkdtempSync('ember-fontawesome-pack-')}`
     const entryFileName = 'free-solid-entry.js'
-    const entryFilePath = `${libTemp}/${entryFileName}`
+    const entryFilePath = `${this._tmpDir}/${entryFileName}`
     fs.appendFileSync(entryFilePath, packageContents)
 
-    // @TODO: pass in a MergeTrees thing?
-    debugger
-    const rollupResults = new Rollup(libTemp, {
+    const merged = new MergeTrees([
+      new Funnel(this._tmpDir),
+      new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome-free-solid')), {
+        destDir: '@fortawesome/fontawesome-free-solid'
+      }),
+    ])
+
+    const rollupNode = new Rollup(merged, {
       rollup: {
         input: entryFileName,
         output: {
           file: 'fas-rolled.js',
           format: 'cjs'
-        }
-      //  plugins: [
-      //    alias({
-      //      '@fortawesome/fontawesome-free-solid': '@fortawesome/fontawesome-free-solid/shakable.es.js'
-      //    })
-      //  ]
+        },
+        plugins: [
+          resolve()
+        ]
       },
       name: 'fontawesome-free-solid-rollup'
     })
@@ -59,64 +47,21 @@ module.exports = {
       new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome')), {
         destDir: '@fortawesome/fontawesome'
       }),
-      new Funnel(path.dirname(require.resolve('@fortawesome/fontawesome-free-solid')), {
-        destDir: '@fortawesome/fontawesome-free-solid'
-      }),
-      rollupResults
+      rollupNode
     ]), {name: 'ember-fontawesome-vendor-tree'})
+  },
+  
+  postBuild() {
+    this.ui.writeLine(`attempting to delete ${this._tmpDir}`)
+    // @TODO: figure out why this isn't deleting
+    rimraf(this._tmpDir, () => {this.ui.writeLine(`failed to remove temp dir: ${this._tmpDir}`)});
   },
 
   included(app) {
     this._super.included.apply(this, arguments)
     this.ui.writeLine('in ember-fontawesome:included()')
     this.fontawesome = app.options.fontawesome
-    //debugger
     app.import('vendor/shims/fontawesome-shim.js')
   },
 }
 
-function memory (config = {}) {
-  let filePath = isPath(config.path) ? config.path : null
-  let contents = isContents(config.contents) ? String(config.contents) : null
-
-  return {
-    options (options) {
-      debugger
-      const { input } = options
-      if (input && typeof input === 'object') {
-        if (isPath(input.path)) {
-          filePath = input.path
-        }
-        if (isContents(input.contents)) {
-          contents = String(input.contents)
-        }
-      }
-      options.input = filePath
-    },
-
-    resolveId (id) {
-      debugger
-      if (filePath === null || contents === null) {
-        throw Error('\'path\' should be a string and \'contents\' should be a string of Buffer')
-      }
-      if (id === filePath) {
-        return path.resolve(filePath)
-      }
-    },
-
-    load (id) {
-      debugger
-      if (id === path.resolve(filePath)) {
-        return contents
-      }
-    }
-  }
-}
-
-function isPath (path) {
-  return typeof path === 'string'
-}
-
-function isContents (contents) {
-  return typeof contents === 'string' || Buffer.isBuffer(contents)
-}
