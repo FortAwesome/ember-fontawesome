6fee6430e2af46dd4a9612ec8d0b0bb49cc1c3ac
Mike Wilkerson <mwilkerson@gmail.com>
2018-02-14 06:48:10 -0800

    empty the blueprint. we do not need anything in there at this time.

--- blueprints/ember-fontawesome/index.js
+++ blueprints/ember-fontawesome/index.js
@@ -2,13 +2,5 @@ module.exports = {
   normalizeEntityName() {},
 
   afterInstall() {
-    return this.addPackagesToProject([
-      {name: '@fortawesome/fontawesome', target: '1.2.0-2'},
-      'broccoli-rollup',
-      'broccoli-stew',
-      'broccoli-funnel',
-      'broccoli-merge-trees',
-      'broccoli-plugin'
-    ]); // is a promise
   }
 };
