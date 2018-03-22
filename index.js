31740e3ec143ff124d2fac93cd9a2503757e2b06
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-03-22 15:25:17 -0700

    Do a better job of discovering the app we belong to (#22)

--- index.js
+++ index.js
@@ -124,9 +124,12 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments)
 
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app
-    }
+    let current = this;
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
 
     this.app = app
 
