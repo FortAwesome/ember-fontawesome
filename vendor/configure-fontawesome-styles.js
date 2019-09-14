124738e6869ab1d7509307d1b600d3d7b7e91f7e
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-09-13 23:49:12 -0700

    Don't add styles to application twice
    We needed to switch this off in the application itself and not just in
    the build to ensure these styles didn't get added twice (they are
    already included in vendor.css).

---
+++ vendor/configure-fontawesome-styles.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var { config } = require('@fortawesome/fontawesome-svg-core');
+
+// turn off adding styles to the app's HTML as these styles are included in the build already
+config.autoAddCss = false;
