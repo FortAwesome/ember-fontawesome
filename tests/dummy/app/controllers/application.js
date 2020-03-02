922de001a84b5713c38db509d0836b42c6dfe9ac
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-03-02 12:40:20 -0800

    Fixup linting issues in the dummy app
    Moved to octane style controller and template for the dummy app.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,8 +1,18 @@
 import Controller from '@ember/controller'
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  init() {
-    this._super(...arguments)
-    this.set('magic', 0)
+export default class ApplicationController extends Controller {
+  @tracked isSpinning = false;
+  @tracked isPulsing = false;
+  @tracked magic = 0;
+
+  @action
+  toggleSpinning() {
+    this.isSpinning = !this.isSpinning;
   }
-});
+  @action
+  togglePulsing() {
+    this.isPulsing = !this.isPulsing;
+  }
+}
