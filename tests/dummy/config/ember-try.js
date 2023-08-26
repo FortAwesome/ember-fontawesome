c56699d5adf995e1f2cb0b0e59fcbff34858cde6
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-08-26 10:39:14 +0200

    Update ember-cli to latest v4.12 * drop support for node < 14 * drop support for ember < 4 * remove ember-try scenarios

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,42 +8,6 @@ module.exports = async function () {
     useNpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.20.0',
-            '@ember/test-helpers': '^2.9.3',
-            'ember-page-title': '^7.0.0',
-            'ember-qunit': '^5.1.4',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.24.0',
-            '@ember/test-helpers': '^2.9.3',
-            'ember-page-title': '^7.0.0',
-            'ember-qunit': '^5.1.4',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.28.0',
-            '@ember/test-helpers': '^2.9.3',
-            'ember-page-title': '^7.0.0',
-            'ember-qunit': '^6.2.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
@@ -89,46 +53,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.28.0',
-            '@ember/test-helpers': '^2.9.3',
-            'ember-page-title': '^7.0.0',
-            'ember-qunit': '^6.2.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-resolver': '^8.0.0',
-            'ember-source': '~3.28.0',
-            '@ember/test-helpers': '^2.9.3',
-            'ember-page-title': '^7.0.0',
-            'ember-qunit': '^6.2.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },
