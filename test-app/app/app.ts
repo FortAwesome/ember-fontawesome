210b790cfb39376f25086c4af7298743b9af19b4
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-05-13 09:20:58 +0200

    Update test-app v6.1.0...v6.4.0

--- test-app/app/app.ts
+++ test-app/app/app.ts
@@ -2,7 +2,12 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'test-app/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import './font-awesome';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
